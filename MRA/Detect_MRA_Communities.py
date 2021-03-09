a4c5cc48855a70f2c305feca6e253f2e161ed952
Alexey <htech.alexey@gmail.com>
2021-03-09 19:48:26 +0200

    NMI with normal signals

--- MRA/Detect_MRA_Communities.py
+++ MRA/Detect_MRA_Communities.py
@@ -14,266 +14,80 @@ def extract_communities_list(communities_list, N):
             community_labels[node] = comm_index
     return community_labels
 
+def plot_nmi_scores(N, sigma_list, samples_num, MRA_type):
+    nmi_scores = {"leiden": [], "louvain": [], "leading_eigenvector": [], "multilevel": [], "spinglass": [],
+                  "walktrap": []}
+    for sigma in sigma_list:
+        nmi_samples = {"leiden": [], "louvain": [], "leading_eigenvector": [], "multilevel": [], "spinglass": [],
+                       "walktrap": []}
+
+        print("sigma: {}".format(sigma))
+
+        # Calculate NMI score for a number (samples_num) of random graphs
+        for i in range(samples_num):
+            # Generate appropriate MRA graph
+            if MRA_type == "Rect_Trian": G, true_labels = MRA_Graphs.MRA_Rect_Trian(N, L=50, K=2, sigma=sigma)
+            elif MRA_type == "Standard Normal": G, true_labels = MRA_Graphs.MRA_StandardNormal(N, L=50, K=10, sigma=sigma)
+            else: G, true_labels = MRA_Graphs.MRA_CorrelatedNormal(N, L=51, K=10, a=1, b=2, choice=1, sigma=sigma)
+
+            print("true: {}".format(true_labels))
+
+            leiden = cd.leiden(G, weights='weight')
+            leiden_labels = extract_communities_list(leiden.communities, N)
+            print("leiden: {}".format(leiden_labels))
+            nmi_samples["leiden"].append(normalized_mutual_info_score(true_labels, leiden_labels))
+
+            louvain = cd.louvain(G, weight='weight')
+            louvain_labels = extract_communities_list(louvain.communities, N)
+            print("louvain: {}".format(louvain_labels))
+            nmi_samples["louvain"].append(normalized_mutual_info_score(true_labels, louvain_labels))
+
+            # Convert NetworkX graph to Igraph graph
+            G = ig.Graph.from_networkx(G)
+
+            leading_eigenvector = ig.Graph.community_leading_eigenvector(G, weights="weight")
+            print("leading eigenvector: {}".format(leading_eigenvector.membership))
+            nmi_samples["leading_eigenvector"].append(
+                normalized_mutual_info_score(true_labels, leading_eigenvector.membership))
+
+            multilevel = ig.Graph.community_multilevel(G, weights="weight")
+            print("multilevel: {}".format(multilevel.membership))
+            nmi_samples["multilevel"].append(normalized_mutual_info_score(true_labels, multilevel.membership))
+
+            spinglass = ig.Graph.community_spinglass(G, weights="weight")
+            print("spinglass: {}".format(spinglass.membership))
+            nmi_samples["spinglass"].append(normalized_mutual_info_score(true_labels, spinglass.membership))
+
+            walktrap = ig.Graph.community_walktrap(G, weights="weight")
+            walktrap = walktrap.as_clustering()
+            print("walktrap: {}".format(walktrap.membership))
+            nmi_samples["walktrap"].append(normalized_mutual_info_score(true_labels, walktrap.membership))
+
+            # Set NMI score for each algrorithm
+        nmi_scores["leiden"].append(np.mean(nmi_samples["leiden"]))
+        nmi_scores["louvain"].append(np.mean(nmi_samples["louvain"]))
+        nmi_scores["leading_eigenvector"].append(np.mean(nmi_samples["leading_eigenvector"]))
+        nmi_scores["multilevel"].append(np.mean(nmi_samples["multilevel"]))
+        nmi_scores["spinglass"].append(np.mean(nmi_samples["spinglass"]))
+        nmi_scores["walktrap"].append(np.mean(nmi_samples["walktrap"]))
+
+    plt.plot(nmi_scores["leiden"], label="leiden")
+    plt.plot(nmi_scores["louvain"], label="louvain")
+    plt.plot(nmi_scores["leading_eigenvector"], label="leading eigenvector")
+    plt.plot(nmi_scores["multilevel"], label="multilevel")
+    plt.plot(nmi_scores["spinglass"], label="spinglass")
+    plt.plot(nmi_scores["walktrap"], label="walktrap")
+    plt.xticks(list(range(len(sigma_list))), sigma_list)
+    plt.title("{0} MRA".format(MRA_type))
+    plt.xlabel("Noise level (sigma)")
+    plt.ylabel("NMI")
+    plt.legend()
+    plt.show()
+
 sigma_list = np.array(range(11))/10
 N = 100
 samples_num = 10
 
-################## Rectangle&Triangle MRA ##################
-nmi_scores = {"greedy": [], "leiden": [], "louvain": [], "cpm": [], "lpa": [], "infomap": [],
-              "leading_eigenvector": [], "multilevel": [], "edge_betweenness": [], "spinglass": [], "walktrap": []}
-
-for sigma in sigma_list:
-    nmi_samples = {"greedy": [], "leiden": [], "louvain": [], "cpm": [], "lpa": [], "infomap": [],
-              "leading_eigenvector": [], "multilevel": [], "edge_betweenness": [], "spinglass": [], "walktrap": []}
-
-    print("sigma: {}".format(sigma))
-
-    # Calculate NMI score for a number (samples_num) of random graphs
-    for i in range(samples_num):
-        G, true_labels = MRA_Graphs.MRA_Rect_Trian(N, L=50, K=2, sigma=sigma)
-        print("true: {}".format(true_labels))
-
-        # Partition Graph by various methods
-        #greedy = cd.greedy_modularity(G, weight='weight')
-        #greedy_labels = extract_communities_list(greedy.communities, N)
-        #print("greedy: {}".format(greedy_labels))
-        #nmi_samples["greedy"].append(normalized_mutual_info_score(true_labels, greedy_labels))
-
-        leiden = cd.leiden(G, weights='weight')
-        leiden_labels = extract_communities_list(leiden.communities, N)
-        print("leiden: {}".format(leiden_labels))
-        nmi_samples["leiden"].append(normalized_mutual_info_score(true_labels, leiden_labels))
-
-        louvain = cd.louvain(G, weight='weight')
-        louvain_labels = extract_communities_list(louvain.communities, N)
-        print("louvain: {}".format(louvain_labels))
-        nmi_samples["louvain"].append(normalized_mutual_info_score(true_labels, louvain_labels))
-
-        #cpm = cd.cpm(G, weights='weight')
-        #cpm_labels = extract_communities_list(cpm.communities, N)
-        #print("cpm: {}".format(cpm_labels))
-        #nmi_samples["cpm"].append(normalized_mutual_info_score(true_labels, cpm_labels))
-
-        #lpa = nx_cd.asyn_lpa_communities(G, weight='weight')
-        #lpa_labels = extract_communities_list(list(lpa), N)
-        #print("lpa: {}".format(lpa_labels))
-        #nmi_samples["lpa"].append(normalized_mutual_info_score(true_labels, lpa_labels))
-
-        #Convert NetworkX graph to Igraph graph
-        G = ig.Graph.from_networkx(G)
-
-        #infomap = ig.Graph.community_infomap(G)
-        #print("infomap: {}".format(infomap.membership))
-        #nmi_samples["infomap"].append(normalized_mutual_info_score(true_labels, infomap.membership))
-
-        leading_eigenvector = ig.Graph.community_leading_eigenvector(G, weights="weight")
-        print("leading eigenvector: {}".format(leading_eigenvector.membership))
-        nmi_samples["leading_eigenvector"].append(normalized_mutual_info_score(true_labels, leading_eigenvector.membership))
-
-        multilevel = ig.Graph.community_multilevel(G, weights="weight")
-        print("multilevel: {}".format(multilevel.membership))
-        nmi_samples["multilevel"].append(normalized_mutual_info_score(true_labels, multilevel.membership))
-
-        #edge_betweenness = ig.Graph.community_edge_betweenness(G, weights="weight")
-        #edge_betweenness = edge_betweenness.as_clustering()
-        #print("edge betweenness: {}".format(edge_betweenness.membership))
-        #nmi_samples["edge_betweenness"].append(normalized_mutual_info_score(true_labels, edge_betweenness.membership))
-
-        spinglass = ig.Graph.community_spinglass(G, weights="weight")
-        print("spinglass: {}".format(spinglass.membership))
-        nmi_samples["spinglass"].append(normalized_mutual_info_score(true_labels, spinglass.membership))
-
-        walktrap = ig.Graph.community_walktrap(G, weights="weight")
-        walktrap = walktrap.as_clustering()
-        print("walktrap: {}".format(walktrap.membership))
-        nmi_samples["walktrap"].append(normalized_mutual_info_score(true_labels, walktrap.membership))
-
-    # Set NMI score for each algrorithm
-    #nmi_scores["greedy"].append(np.mean(nmi_samples["greedy"]))
-    nmi_scores["leiden"].append(np.mean(nmi_samples["leiden"]))
-    nmi_scores["louvain"].append(np.mean(nmi_samples["louvain"]))
-    #nmi_scores["cpm"].append(np.mean(nmi_samples["cpm"]))
-    #nmi_scores["lpa"].append(np.mean(nmi_samples["lpa"]))
-    #nmi_scores["infomap"].append(np.mean(nmi_samples["infomap"]))
-    nmi_scores["leading_eigenvector"].append(np.mean(nmi_samples["leading_eigenvector"]))
-    nmi_scores["multilevel"].append(np.mean(nmi_samples["multilevel"]))
-    #nmi_scores["edge_betweenness"].append(np.mean(nmi_samples["edge_betweenness"]))
-    nmi_scores["spinglass"].append(np.mean(nmi_samples["spinglass"]))
-    nmi_scores["walktrap"].append(np.mean(nmi_samples["walktrap"]))
-
-#plt.plot(nmi_scores["greedy"],label="greedy")
-plt.plot(nmi_scores["leiden"],label="leiden")
-plt.plot(nmi_scores["louvain"],label="louvain")
-#plt.plot(nmi_scores["cpm"],label="CPM")
-#plt.plot(nmi_scores["lpa"],label="LPA")
-#plt.plot(nmi_scores["infomap"],label="infomap")
-plt.plot(nmi_scores["leading_eigenvector"],label="leading eigenvector")
-plt.plot(nmi_scores["multilevel"],label="multilevel")
-#plt.plot(nmi_scores["edge_betweenness"],label="edge betweenness")
-plt.plot(nmi_scores["spinglass"],label="spinglass")
-plt.plot(nmi_scores["walktrap"],label="walktrap")
-plt.xticks(list(range(len(sigma_list))), sigma_list)
-plt.title("Triangle&Rectangle MRA")
-plt.xlabel("Noise level (sigma)")
-plt.ylabel("NMI")
-plt.legend()
-plt.show()
-'''
-################## Standard Normal MRA ##################
-greedy_nmi = []
-leiden_nmi = []
-louvain_nmi = []
-cpm_nmi = []
-lpa_nmi = []
-infomap_nmi = []
-leading_eigenvector_nmi = []
-multilevel_nmi = []
-edge_betweenness_nmi = []
-spinglass_nmi = []
-walktrap_nmi = []
-
-for sigma in sigma_list:
-    G, true_labels = MRA_Graphs.MRA_StandardNormal(N, L=50, K=2, sigma=sigma)
-
-    # Partition Graph by various methods
-    greedy = cd.greedy_modularity(G, weight='weight')
-    greedy_labels = extract_communities_list(greedy.communities, N)
-    greedy_nmi.append(normalized_mutual_info_score(true_labels, greedy_labels))
-
-    leiden = cd.leiden(G, weights='weight')
-    leiden_labels = extract_communities_list(leiden.communities, N)
-    leiden_nmi.append(normalized_mutual_info_score(true_labels, leiden_labels))
-
-    louvain = cd.louvain(G, weight='weight')
-    louvain_labels = extract_communities_list(louvain.communities, N)
-    louvain_nmi.append(normalized_mutual_info_score(true_labels, louvain_labels))
-
-    cpm = cd.cpm(G, weights='weight')
-    cpm_labels = extract_communities_list(cpm.communities, N)
-    cpm_nmi.append(normalized_mutual_info_score(true_labels, cpm_labels))
-
-    lpa = nx_cd.asyn_lpa_communities(G, weight='weight')
-    lpa_labels = extract_communities_list(list(lpa), N)
-    lpa_nmi.append(normalized_mutual_info_score(true_labels, lpa_labels))
-
-    # Convert NetworkX graph to Igraph graph
-    G = ig.Graph.from_networkx(G)
-
-    infomap = ig.Graph.community_infomap(G, edge_weights="weight")
-    infomap_nmi.append(normalized_mutual_info_score(true_labels, infomap.membership))
-
-    leading_eigenvector = ig.Graph.community_leading_eigenvector(G, weights="weight")
-    leading_eigenvector_nmi.append(normalized_mutual_info_score(true_labels, leading_eigenvector.membership))
-
-    multilevel = ig.Graph.community_multilevel(G, weights="weight")
-    multilevel_nmi.append(normalized_mutual_info_score(true_labels, multilevel.membership))
-
-    edge_betweenness = ig.Graph.community_edge_betweenness(G, weights="weight")
-    edge_betweenness = edge_betweenness.as_clustering()
-    edge_betweenness_nmi.append(normalized_mutual_info_score(true_labels, edge_betweenness.membership))
-
-    spinglass = ig.Graph.community_spinglass(G, weights="weight")
-    spinglass_nmi.append(normalized_mutual_info_score(true_labels, spinglass.membership))
-
-    walktrap = ig.Graph.community_walktrap(G, weights="weight")
-    walktrap = walktrap.as_clustering()
-    walktrap_nmi.append(normalized_mutual_info_score(true_labels, walktrap.membership))
-
-plt.plot(greedy_nmi, label="greedy")
-plt.plot(leiden_nmi, label="leiden")
-plt.plot(louvain_nmi, label="louvain")
-plt.plot(cpm_nmi, label="CPM")
-plt.plot(lpa_nmi, label="LPA")
-plt.plot(infomap_nmi, label="infomap")
-plt.plot(leading_eigenvector_nmi, label="leading eigenvector")
-plt.plot(multilevel_nmi, label="multilevel")
-plt.plot(edge_betweenness_nmi, label="edge betweenness")
-plt.plot(spinglass_nmi, label="spinglass")
-plt.plot(walktrap_nmi, label="walktrap")
-plt.xticks(list(range(len(sigma_list))), sigma_list)
-plt.title("Standard Normal MRA")
-plt.xlabel("Noise level (sigma)")
-plt.ylabel("NMI")
-plt.legend()
-plt.show()
-'''
-'''
-################## Random Normal MRA ##################
-greedy_nmi = np.zeros((10, 10))
-leiden_nmi = []
-louvain_nmi = []
-cpm_nmi = []
-lpa_nmi = []
-infomap_nmi = []
-leading_eigenvector_nmi = []
-multilevel_nmi = []
-edge_betweenness_nmi = []
-spinglass_nmi = []
-walktrap_nmi = []
-
-for sigma in sigma_list:
-    for i in range(10):
-        G, true_labels = MRA_Graphs.MRA_RandomNormal(N, L=50, K=2, sigma=sigma)
-
-        # Partition Graph by various methods
-        greedy = cd.greedy_modularity(G, weight='weight')
-        greedy_labels = extract_communities_list(greedy.communities, N)
-        greedy_nmi.append(normalized_mutual_info_score(true_labels, greedy_labels))
-
-    leiden = cd.leiden(G, weights='weight')
-    leiden_labels = extract_communities_list(leiden.communities, N)
-    leiden_nmi.append(normalized_mutual_info_score(true_labels, leiden_labels))
-
-    louvain = cd.louvain(G, weight='weight')
-    louvain_labels = extract_communities_list(louvain.communities, N)
-    louvain_nmi.append(normalized_mutual_info_score(true_labels, louvain_labels))
-
-    cpm = cd.cpm(G, weights='weight')
-    cpm_labels = extract_communities_list(cpm.communities, N)
-    cpm_nmi.append(normalized_mutual_info_score(true_labels, cpm_labels))
-
-    lpa = nx_cd.asyn_lpa_communities(G, weight='weight')
-    lpa_labels = extract_communities_list(list(lpa), N)
-    lpa_nmi.append(normalized_mutual_info_score(true_labels, lpa_labels))
-
-    # Convert NetworkX graph to Igraph graph
-    G = ig.Graph.from_networkx(G)
-
-    infomap = ig.Graph.community_infomap(G, edge_weights="weight")
-    infomap_nmi.append(normalized_mutual_info_score(true_labels, infomap.membership))
-
-    leading_eigenvector = ig.Graph.community_leading_eigenvector(G, weights="weight")
-    leading_eigenvector_nmi.append(normalized_mutual_info_score(true_labels, leading_eigenvector.membership))
-
-    multilevel = ig.Graph.community_multilevel(G, weights="weight")
-    multilevel_nmi.append(normalized_mutual_info_score(true_labels, multilevel.membership))
-
-    edge_betweenness = ig.Graph.community_edge_betweenness(G, weights="weight")
-    edge_betweenness = edge_betweenness.as_clustering()
-    edge_betweenness_nmi.append(normalized_mutual_info_score(true_labels, edge_betweenness.membership))
-
-    spinglass = ig.Graph.community_spinglass(G, weights="weight")
-    spinglass_nmi.append(normalized_mutual_info_score(true_labels, spinglass.membership))
-
-    walktrap = ig.Graph.community_walktrap(G, weights="weight")
-    walktrap = walktrap.as_clustering()
-    walktrap_nmi.append(normalized_mutual_info_score(true_labels, walktrap.membership))
-
-plt.plot(greedy_nmi, label="greedy")
-plt.plot(leiden_nmi, label="leiden")
-plt.plot(louvain_nmi, label="louvain")
-plt.plot(cpm_nmi, label="CPM")
-plt.plot(lpa_nmi, label="LPA")
-plt.plot(infomap_nmi, label="infomap")
-plt.plot(leading_eigenvector_nmi, label="leading eigenvector")
-plt.plot(multilevel_nmi, label="multilevel")
-plt.plot(edge_betweenness_nmi, label="edge betweenness")
-plt.plot(spinglass_nmi, label="spinglass")
-plt.plot(walktrap_nmi, label="walktrap")
-plt.xticks(list(range(len(sigma_list))), sigma_list)
-plt.title("Random Normal MRA")
-plt.xlabel("Noise level (sigma)")
-plt.ylabel("NMI")
-plt.legend()
-plt.show()
-'''
+#plot_nmi_scores(N, sigma_list, samples_num, "Rect_Trian")
+plot_nmi_scores(N, sigma_list, samples_num, "Standard_Normal")
+plot_nmi_scores(N, sigma_list, samples_num, "Correlated_Normal")
--- MRA/MRA_Graphs.py
+++ MRA/MRA_Graphs.py
@@ -36,13 +36,51 @@ def MRA_StandardNormal(N, L, K, sigma):
 
     return G, true_partition
 
-def MRA_RandomNormal(N, L, K, sigma):
+#Generate Correlated signals functions:
+
+def DFT_matrix(N):
+    i, j = np.meshgrid(np.arange(N), np.arange(N))
+    omega = np.exp( - 2 * np.pi * 1J / N )
+    W = np.power( omega, i * j ) / np.sqrt(N)
+    return W
+
+def create_arr(L):
+    return (np.arange(1, 1+ (L+1) / 2))
+
+def apply_function(arr,choice,a,b): #'1' for a*x^(-b), '2' for a*b^(-x)
+    if (choice==1):
+        PSD_Oneside=a * (1 / ((arr) ** b))
+    else:
+       PSD_Oneside= a * (b**((-1)* arr))
+    return PSD_Oneside
+
+def create_covariance_matrix(L,choice,a,b): #'1' for a*x^(-b), '2' for a*b^(-x)
+    F=DFT_matrix(L)
+    arry = create_arr(L)
+    PSD_oneside=apply_function(arry,choice,a,b)
+    PSD=np.concatenate([PSD_oneside,PSD_oneside[1::][::-1]]) #change here [2::] for even L
+    D=np.diag(PSD)
+    F_conju=np.conj(F)
+    F_transposy=F_conju.T
+    Sigma=np.real(F_transposy @ D @ F)
+    return Sigma
+
+# Generate one signal with covariance by known function
+def generate_a_signal(L,a,b,choice):
+    if(b==1):
+        sigi=np.random.randn(L)
+    else:
+        Sigma=create_covariance_matrix(L,choice,a,b)
+        sigi = np.random.multivariate_normal(np.zeros((L,)),Sigma)
+    return sigi
+
+def MRA_CorrelatedNormal(N, L, K, a, b, choice, sigma):
     x = np.zeros((K, L))
-    random_std = stats.randint.rvs(low=1, high=5, size=K)  # Random uniformly distributed selections of signals
+
     # Generate Standard Normally Distributed signals
     for k in range(K):
-        x[k] = np.random.normal(0, random_std[k], L)
-        x[k] = (x[k] - np.mean(x[k])) / np.linalg.norm(x[k] - np.mean(x[k]), 2)  # Normalize signal
+        x[k] = generate_a_signal(L, a, b, choice)
+        x[k] = (x[k] - np.mean(x[k])) / np.linalg.norm(x[k] - np.mean(x[k]), 2) # Normalize signal
 
     y, true_partition = Generate.generate_MRA(N, K, L, sigma, x)
     max_corr = Generate.generate_maxcorr(N, L, y)
